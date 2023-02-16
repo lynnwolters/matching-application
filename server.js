c295509671121688ec966d0dbcba4bdf48f1791c
Lynn Wolters <lynn.wolters27@gmail.com>
2023-02-16 15:08:57 +0100

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000
 
-app.get('/', onHome).listen(PORT, console.log('Runnning on port: ${PORT}'));
+app.get('/', onHome).listen(PORT, console.log('Running on port: ${PORT}'));
 
 function onHome(req, res) {
     res.send('Hallo')
