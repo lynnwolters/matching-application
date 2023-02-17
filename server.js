ff6c4961827afe7cb17273989a0fcd7ef1dc1b3e
Lynn Wolters <lynn.wolters27@gmail.com>
2023-02-17 12:52:45 +0100

    begonnen met bouwen van ontwerp in html/css

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 8000
 app.get('/', onHome).listen(PORT, console.log('Running on port: ${PORT}'));
 
 function onHome(req, res) {
-    res.send('Hallo')
+    res.send('')
 }
