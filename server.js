0f8cb2bc254a1ff7607f0c7724056c2243babf21
Lynn Wolters <lynn.wolters27@gmail.com>
2023-02-20 17:18:56 +0100

    20-02-2023 / 17:18

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000
+const path = require('path');
 
-app.get('/', onHome).listen(PORT, console.log('Running on port: ${PORT}'));
+app.use(express.static("static"));
 
+app.get('/index', onHome);
 function onHome(req, res) {
-    res.send('')
+    res.sendFile(path.join(__dirname, 'view/index.html'));
 }
+
+app.get('/profiel-bewerken', profielBewerken);
+function profielBewerken(req, res) {
+    res.sendFile(path.join(__dirname, 'view/profiel-bewerken.html'));
+}
+
+app.listen(PORT, console.log('Running on port: ${PORT}'));
