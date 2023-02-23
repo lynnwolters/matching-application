02420bf67fc55846894808f7db6fb856d2455413
Lynn Wolters <lynn.wolters27@gmail.com>
2023-02-23 11:06:30 +0100

    v6

--- server.js
+++ server.js
@@ -3,16 +3,28 @@ const app = express();
 const PORT = process.env.PORT || 8000
 const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views' + '/views');
+
+app.get('/', function(req, res) {
+    res.render('index', { title: 'My Website' });
+});
+
 app.use(express.static("static"));
 
 app.get('/index', onHome);
 function onHome(req, res) {
-    res.sendFile(path.join(__dirname, 'view/index.html'));
+    res.sendFile('view/index.ejs');
 }
 
 app.get('/profiel-bewerken', profielBewerken);
 function profielBewerken(req, res) {
-    res.sendFile(path.join(__dirname, 'view/profiel-bewerken.html'));
+    res.sendFile('view/profiel-bewerken.ejs');
 }
 
 app.listen(PORT, console.log('Running on port: ${PORT}'));
+
+app.use((req, res, next) => {
+    res.status(404).send(
+        "<h1>Page not found on the server</h1>")
+})
