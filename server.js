60c051d221d61a19d451dc2b58424f8886288409
Lynn Wolters <lynn.wolters27@gmail.com>
2023-03-06 12:38:00 +0100

    06-03-2023, 12:37, comments toegevoegd

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+// NODIG OM EXPRESS TE KUNNEN GEBRUIKEN
 const express = require('express');
 const app = express();
+
+// LOCALHOST PORT
 const PORT = process.env.PORT || 8000
+
+// NODIG OM DATA OP EEN DYNAMISCHE MANIER IN TE LADEN
 const path = require('path');
 const fs = require('fs');
 const jsonData = fs.readFileSync('static/data/data.json', 'utf8');
 const myData = JSON.parse(jsonData);
 
+//  NODIG OM .ENV BESTAND IN TE LADEN
 require('dotenv').config()
 
+// MONGO DB CONNECTIE 
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = process.env.MONGO_URI
+const uri = 'mongodb+srv://<username>:<{encodedOptionParam}>@matching-application.wmyg2ya.mongodb.net/?retryWrites=true&w=majority'
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -22,11 +29,14 @@ client.connect(err => {
     }
 })
 
+// VIEW ENGINES INLADEN
 app.set('view engine', 'ejs');
 app.set('views', 'view');
 
+// STATIC CONTENT INLADEN
 app.use(express.static("static"));
 
+// INDEX.EJS INLADEN
 app.get('/index', onHome);
 function onHome(req, res) {
     res.render('index.ejs', {
@@ -34,14 +44,17 @@ function onHome(req, res) {
     })
 }
 
+// PROFIEL-BEWERKEN.EJS INLADEN
 app.get('/profiel-bewerken', profielBewerken);
 function profielBewerken(req, res) {
     res.render('profiel-bewerken.ejs');
 }
 
-app.listen(PORT, console.log('Running on port: ${PORT}'));
-
+// 404 PAGE
 app.use((req, res, next) => {
     res.status(404).send(
-        "<h1>Page not found on the server</h1>")
+        "<h1>Deze pagina kan niet gevonden worden.</h1>")
 })
+
+// LOCALHOST LATEN WERKEN
+app.listen(PORT, console.log('Running on port: ${PORT}'));
