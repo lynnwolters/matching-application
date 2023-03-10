e21650d591e8170727c84a84bf78b8e3320164ce
Lynn Wolters <lynn.wolters27@gmail.com>
2023-03-10 16:53:21 +0100

    16:53

---
+++ static/js/save-button.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+	const saveButton = document.querySelector('button[name="submit"]')
+	const inputFields = document.querySelectorAll('input')
+
+	let disabled = true
+	saveButton.disabled = disabled
+
+	inputFields.forEach((inputField) => {
+		inputField.addEventListener('input', () => {
+
+			let hasValue = false
+			inputFields.forEach((field) => {
+				if (field.value.trim() !== '') {
+					hasValue = true
+				}
+			})
+
+			if (hasValue) {
+				disabled = false
+				saveButton.style.backgroundColor = 'green'
+			} else {
+				disabled = true
+				saveButton.style.backgroundColor = ''
+			}
+
+			saveButton.disabled = disabled
+		})
+	})
+}
